1c4d1091e7468f481057632579bde27628c79097
Chalo Salvador <chalosalvador@gmail.com>
2017-05-09 19:00:14 -0500

    ejercicios deber

--- ejercicio4/index.php
+++ ejercicio4/index.php
@@ -6,7 +6,7 @@
  * El formulario debe solicitar el nombre de usuario y la clave en dos oportunidades. 
  * El formulario debe ser procesado por el archivo validarRegistro.php. Este archivo
  * debe implementar una función que permita validar si las dos contraseñas coinciden. 
- * Si las contraseñas coinciden, se debe redirigir al archivo "regitroCorrecto.php"
+ * Si las contraseñas coinciden, se debe redirigir al archivo "registroCorrecto.php"
  * Si las contraseñas no coinciden, se debe redirigir al index.php y mostrar el mensaje
  * "Las contrseñas no coinciden".
  */
